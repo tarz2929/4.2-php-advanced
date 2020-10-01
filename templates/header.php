c25745bb04d3d1c542367f85b4587c3c708fb080
tarz2929 <saitarunmulley@gmail.com>
2020-10-01 10:51:45 -0600

    First commit/initial setup

---
+++ templates/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <!-- Style(s) -->
+    <link rel="stylesheet" type ="text/css" href="css/main.css">
+
+    <!-- Script(s) -->
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+
+</head>
+<body>
+<h1>Webpage</h1>
+
+    
+
