7347c8f58a400916950c1cf366e29ae092f968cc
tarz2929 <saitarunmulley@gmail.com>
2020-10-01 12:01:21 -0600

    Add a calculator basics

--- calc.php
+++ calc.php
@@ -10,8 +10,47 @@ include './templates/header.php';
   Welcome to our Calculator page!
 </p>
 
-<form>
+<form method="GET">
+<label for ="num1">
+    Enter first operand:
+    <input
+        id="num1"
+        name="value1"
+        type="number"
+        value="">
+  </label>
 
+  <label for="operator">
+    Select an operator:
+
+    <Select id="operator" name="op">
+    <option value="addition">
+    +
+    </option>
+    <option value="subtraction">
+    -
+    </option>
+    <option value="multiplication">
+    &times;
+    </option>
+    <option value="division">
+    &divide;
+    </option>
+
+
+
+    </Select>
+
+  <label for ="num2">
+    Enter second operand:
+    <input
+        id="num2"
+        name="value2"
+        type="number"
+        value="">
+  </label>
+
+  <input type="submit" value="Calculate!">
 
 </form>
 
