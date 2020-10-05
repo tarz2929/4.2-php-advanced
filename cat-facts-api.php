1a1847acfe7c8fad3d4230661b5e779f69b02575
tarz2929 <saitarunmulley@gmail.com>
2020-10-05 11:07:58 -0600

    Display the fact text on the browser

--- cat-facts-api.php
+++ cat-facts-api.php
@@ -1,7 +1,22 @@
 <?php
 $GLOBALS['pageTitle'] = 'Cat Facts using an API';
 include './templates/header.php';
-?>
 
 
-<? include './templates/footer.php';
+$dailyCatFactResponse = file_get_contents('https://cat-fact.herokuapp.com/facts/random');
+
+// var_dump( $dailyCatFactResponse);
+
+if ( $dailyCatFactResponse)
+{
+    $dailyCatFact = json_decode( $dailyCatFactResponse);
+    ?>
+    <h2>Today's Cat Fact:</h2>
+    <p><?php echo $dailyCatFact->text; ?> </p>   
+
+    <?php
+}
+
+
+
+include './templates/footer.php';
